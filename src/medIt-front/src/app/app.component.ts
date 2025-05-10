85d7991695d8029c96ff4676a330f7cc77324ddd
adam <adam.laftimi@gmail.com>
2025-05-10 21:24:54 +0100

    angular spring boot stack

--- src/medIt-front/src/app/app.component.ts
+++ src/medIt-front/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { environment } from '../environments/environment';
+import { LoginComponent } from "./homePage/login/login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
